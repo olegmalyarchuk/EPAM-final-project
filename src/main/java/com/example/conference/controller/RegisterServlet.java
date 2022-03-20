eecc3f969d30f2607242bd664140e48cc0e81d5f
olegmalyarchuk <olegmalyarchuk7@gmail.com>
2022-03-20 23:18:35 +0200

    bug was fixed. Registration page was added

---
+++ src/main/java/com/example/conference/controller/RegisterServlet.java
@@ -0,0 +1,140 @@
+package com.example.conference.controller;
+
+import com.example.conference.entity.User;
+import com.example.conference.exceptions.DBException;
+import com.example.conference.service.IUserService;
+import com.example.conference.service.ServiceFactory;
+import com.example.conference.validator.Validator;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet {
+    public static final long serialVersionUID = 12353252L;
+    IUserService service = ServiceFactory.getInstance().getUserService();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Integer id = service.calculateUsersNumber()+1;
+        Integer role_id = 3;
+        String firstName = req.getParameter("firstname");
+        String lastName = req.getParameter("lastname");
+        String password = req.getParameter("password");
+        String confirmpassword = req.getParameter("confirmpassword");
+        String phone = req.getParameter("phone");
+        String email = req.getParameter("email");
+        String photo = "user.jpg";
+        String address = req.getParameter("address");
+        RequestDispatcher dispatcher = null;
+        int cntEmails = 0;
+        int cntPhones = 0;
+        try {
+           cntEmails = service.calculateRowsBy("user_email", email);
+           cntPhones = service.calculateRowsBy("user_phone", phone);
+        } catch (DBException e) {
+            //e.printStackTrace();
+        }
+
+        //empty
+        if(firstName == null || firstName.equals("")) {
+            req.setAttribute("status", "emptyFirstname");
+            dispatcher = req.getRequestDispatcher("register.jsp");
+            dispatcher.forward(req, resp);
+        }
+        else if(!Validator.isValidName(firstName)) {
+            req.setAttribute("status", "invalidFirstname");
+            dispatcher = req.getRequestDispatcher("register.jsp");
+            dispatcher.forward(req, resp);
+        }
+        else if(lastName == null || lastName.equals("")) {
+            req.setAttribute("status", "emptyLastname");
+            dispatcher = req.getRequestDispatcher("register.jsp");
+            dispatcher.forward(req, resp);
+        }
+        else if(!Validator.isValidName(lastName)) {
+            req.setAttribute("status", "invalidLastname");
+            dispatcher = req.getRequestDispatcher("register.jsp");
+            dispatcher.forward(req, resp);
+        }
+        else if(password == null || password.equals("")) {
+            req.setAttribute("status", "emptyPassword");
+            dispatcher = req.getRequestDispatcher("register.jsp");
+            dispatcher.forward(req, resp);
+        }
+        else if(!Validator.isValidPassword(password)) {
+            req.setAttribute("status", "invalidPassword");
+            dispatcher = req.getRequestDispatcher("register.jsp");
+            dispatcher.forward(req, resp);
+        }
+        else if(!confirmpassword.equals(password)) {
+            req.setAttribute("status", "invalidConfirmpassword");
+            dispatcher = req.getRequestDispatcher("/register.jsp");
+            dispatcher.forward(req, resp);
+        }
+        else if(email == null || email.equals("")) {
+            req.setAttribute("status", "emptyEmail");
+            dispatcher = req.getRequestDispatcher("register.jsp");
+            dispatcher.forward(req, resp);
+        }
+        else if(!Validator.isValidEmail(email)) {
+            req.setAttribute("status", "invalidEmail");
+            dispatcher = req.getRequestDispatcher("register.jsp");
+            dispatcher.forward(req, resp);
+        }
+        else if(cntEmails!=0) {
+            req.setAttribute("status", "dublicateEmail");
+            dispatcher = req.getRequestDispatcher("register.jsp");
+            dispatcher.forward(req, resp);
+
+        }
+       else if(phone == null || phone.equals("")) {
+            req.setAttribute("status", "emptyPhone");
+            dispatcher = req.getRequestDispatcher("register.jsp");
+            dispatcher.forward(req, resp);
+        }
+        else if(!Validator.isValidPhone(phone)) {
+            req.setAttribute("status", "invalidPhone");
+            dispatcher = req.getRequestDispatcher("register.jsp");
+            dispatcher.forward(req, resp);
+        }
+        else if(cntPhones!=0) {
+            req.setAttribute("status", "dublicatePhone");
+            dispatcher = req.getRequestDispatcher("register.jsp");
+            dispatcher.forward(req, resp);
+
+        }
+        else if(address == null || address.equals("")) {
+            req.setAttribute("status", "emptyAddress");
+            dispatcher = req.getRequestDispatcher("register.jsp");
+            dispatcher.forward(req, resp);
+        }
+        else {
+            User newUser = new User();
+            newUser.setId(id);
+            newUser.setRole_id(role_id);
+            newUser.setUser_name(firstName);
+            newUser.setUser_surname(lastName);
+            newUser.setUser_password(password);
+            newUser.setUser_phone(phone);
+            newUser.setUser_email(email);
+            newUser.setUser_photo_url(photo);
+            newUser.setUser_address(address);
+
+            if (service.addUserToDB(newUser)) {
+                req.setAttribute("status", "success");
+                dispatcher = req.getRequestDispatcher("register.jsp");
+                dispatcher.forward(req, resp);
+            } else {
+                req.setAttribute("status", "servererror");
+                dispatcher = req.getRequestDispatcher("register.jsp");
+                dispatcher.forward(req, resp);
+            }
+        }
+    }
+}
